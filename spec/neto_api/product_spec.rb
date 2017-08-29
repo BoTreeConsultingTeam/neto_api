eda10541b2b543b7709a28243b589a97dd52ee5b
Amit Patel <amit.patel@botreeconsulting.com>
2017-08-29 18:04:55 +0530

    Introducing VCR

--- spec/neto_api/product_spec.rb
+++ spec/neto_api/product_spec.rb
@@ -3,10 +3,12 @@ require_relative '../spec_helper'
 describe NetoApi::Product do
   describe '#all' do
     it 'returns all products' do
-      product = NetoApi::Product.new('https://amits-assorted.neto.com.au', '3RqSqZ384PtUNh9Im6A0bAG9FdGbmq0j')
-      response = product.all
-      expect(response.status).to eq 200
-      expect(response.body).not_to be_nil
+      VCR.use_cassette 'products' do
+        product = NetoApi::Product.new('https://amits-assorted.neto.com.au', '3RqSqZ384PtUNh9Im6A0bAG9FdGbmq0j')
+        response = product.all
+        expect(response.status).to eq 200
+        expect(response.body).not_to be_nil
+      end
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'neto_api'
 require 'pry'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
